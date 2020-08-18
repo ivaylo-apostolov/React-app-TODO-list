8f3c621b04281dbfae6e6aecad90471fddb9ec21
ivaylo-apostolov <ivaylo.apostolov@gmail.com>
2020-08-19 00:27:24 +0300

    hooks inital commit

--- todo/src/components/app/App.js
+++ todo/src/components/app/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Input from "../input/Input";
 import Button from "../button/Button";
 import Task from "../task/Task";
 import TasksList from "../tasksList/TasksList";
 import "./App.css";
 
-class App extends Component {
-  state = {
+const App = () => {
+  debugger;
+  const [state, setState] = useState({
     tasks: [
       {
         name: "Initial task",
@@ -16,38 +17,36 @@ class App extends Component {
     ],
     inputContent: "",
     toShow: "TODO",
-  };
+  });
 
-  inputChange = (event) => {
-    let inputValue = this.state.inputContent;
+  const inputChange = (event) => {
+    let inputValue = state.inputContent;
     inputValue = event.target.value;
-    this.setState({ inputContent: inputValue });
+    setState({ inputContent: inputValue });
   };
 
-  addTaskToList = () => {
+  const addTaskToList = () => {
     let newTask = {
-      name: this.state.inputContent,
+      name: state.inputContent,
       status: "TODO",
-      id: this.state.inputContent,
+      id: state.inputContent,
     };
-    let taskExists = this.state.tasks.filter(
-      (task) => task.name === newTask.name
-    );
+    let taskExists = state.tasks.filter((task) => task.name === newTask.name);
     if (!taskExists.length) {
-      this.setState({
-        tasks: [...this.state.tasks, newTask]
+      setState({
+        tasks: [...state.tasks, newTask],
       });
     }
   };
 
-  showChoosenTasks = (choosenTaskName) => {
-    this.setState({
+  const showChoosenTasks = (choosenTaskName) => {
+    setState({
       toShow: choosenTaskName,
     });
   };
 
-  changeStatus = (taskIndex) => {
-    let task = { ...this.state.tasks[taskIndex] };
+  const changeStatus = (taskIndex) => {
+    let task = { ...state.tasks[taskIndex] };
     if (task.status === "TODO") {
       task.status = "In Progress";
     } else if (task.status === "In Progress") {
@@ -55,24 +54,21 @@ class App extends Component {
     } else if (task.status === "Done") {
       task.status = "TODO";
     }
-    const tasksArr = [...this.state.tasks];
+    const tasksArr = [...state.tasks];
     tasksArr[taskIndex] = task;
-    this.setState({
+    setState({
       tasks: tasksArr,
     });
   };
 
-  render() {
-    const tasks = this.state.tasks;
+  const render = () => {
+    const tasks = state.tasks;
     const tasksLst = tasks
-      .filter(
-        (task) =>
-          task.status === this.state.toShow || this.state.toShow === "All"
-      )
+      .filter((task) => task.status === state.toShow || state.toShow === "All")
       .map((task, index) => {
         return (
           <Task
-            click={() => this.changeStatus(index)}
+            click={() => changeStatus(index)}
             key={task.id}
             name={task.name}
             status={task.status}
@@ -81,7 +77,7 @@ class App extends Component {
       });
     let tasksList = (
       <div>
-        <TasksList tasksList={tasksLst} toShow={this.state.toShow} />
+        <TasksList tasksList={tasksLst} toShow={state.toShow} />
       </div>
     );
 
@@ -90,33 +86,26 @@ class App extends Component {
         <h2>TODO list</h2>
         <Input
           changed={(event) => {
-            this.inputChange(event);
+            inputChange(event);
           }}
           labelText="Task"
         />
-        <Button click={this.addTaskToList} btnName="Add" />
+        <Button click={addTaskToList} btnName="Add" />
         {tasksList}
         <div>
+          <Button click={() => showChoosenTasks("All")} btnName="Show All" />
+          <Button click={() => showChoosenTasks("TODO")} btnName="Show TODO" />
           <Button
-            click={() => this.showChoosenTasks("All")}
-            btnName="Show All"
-          />
-          <Button
-            click={() => this.showChoosenTasks("TODO")}
-            btnName="Show TODO"
-          />
-          <Button
-            click={() => this.showChoosenTasks("In Progress")}
+            click={() => showChoosenTasks("In Progress")}
             btnName="Show In Progress"
           />
-          <Button
-            click={() => this.showChoosenTasks("Done")}
-            btnName="Show Done"
-          />
+          <Button click={() => showChoosenTasks("Done")} btnName="Show Done" />
         </div>
       </div>
     );
-  }
-}
+  };
+
+  return render;
+};
 
 export default App;
